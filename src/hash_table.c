39ccbf669dc05adbfa21892e25052e9e637d3e5a
Kerolos-Marsleus <69111144+Kerolos-Marsleus@users.noreply.github.com>
2025-09-15 08:40:42 +0300

    add-delete-search-functions

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
-
 #include "hash_table.h"
+
 
 static ht_item* ht_new_item(const char* k, const char* v){
     ht_item* i = malloc(sizeof(ht_item));
@@ -53,4 +53,65 @@ static int ht_hash(const char* str, const int prime_number, const int ht_size){
         hash = hash % ht_size;
     }
     return (int)hash;
+}
+
+static int ht_get_hash(const char* str, const int num_buckets, const int attempt){
+    const int hash_a = ht_hash(str, HT_PRIME_1, num_buckets);
+    const int hash_b = ht_hash(str, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+
+void ht_insert(ht_hash_table* ht,const char* key,const char* value){
+    if(ht->count >= ht->size){
+        // there is no memory
+        // resize.
+    }
+    ht_item* item = ht_new_item(key, value);
+    int index = ht_get_hash(item->key, ht->size, 0);
+    ht_item* current_item = ht->items[index];
+    int i = 1;
+    while(current_item != NULL){
+        index = ht_get_hash(item->key, ht->size, 1);
+        current_item = ht->items[index];
+        i++;
+    }
+    ht->items[index] = item;
+    ht->count++;
+}
+
+char* ht_search(ht_hash_table* ht, const char* key){
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+    while(item != NULL){
+        if(strcmp(key, ht->items[index]) == 0){
+            return item->value;
+        }
+        index = ht_get_hash(key, ht->size, i);
+        i++;
+        item = ht->items[index];
+    }
+    return NULL;
+}
+
+
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
+void ht_delete(ht_hash_table* ht, const char* key){
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+    while(item != NULL){
+        if(item != &HT_DELETED_ITEM){
+            if(strcmp(key, item->key)==0){
+                ht_del_item(item);
+                ht->items[index] = &HT_DELETED_ITEM;
+            }
+        }
+        index  = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    }
+    ht->count--;
 }
